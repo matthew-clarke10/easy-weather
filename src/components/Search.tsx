616d85471c129b3e0524763ec9478e34129195ef
matthew-clarke10 <matthewkclarke10@gmail.com>
2024-06-25 16:49:38 +1000

    Basic search bar styling.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,12 @@
 function Search() {
   return (
-    <h1>Search</h1>
+    <section className="text-center text-2xl mt-8">
+      <section className="search-bar w-4/5 mx-auto flex border-2">
+        <input className="search-input flex-grow pl-4" type="text" placeholder="e.g. 'Newcastle'" spellCheck="false" />
+        <button className="search-butto p-4 bg-red-100" type="button">Search</button>
+      </section>
+      <div className="error-message text-center"></div>
+    </section>
   )
 }
 
