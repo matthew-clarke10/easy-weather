d20d5549200b985f06d5a88cd58289dc42fad9b6
matthew-clarke10 <matthewkclarke10@gmail.com>
2024-06-25 19:47:56 +1000

    Basic user weather styling.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,11 @@
 function Search() {
   return (
     <section className="text-center text-2xl mt-8">
-      <section className="search-bar w-4/5 mx-auto flex border-2">
-        <input className="search-input flex-grow pl-4" type="text" placeholder="e.g. 'Newcastle'" spellCheck="false" />
-        <button className="search-butto p-4 bg-red-100" type="button">Search</button>
+      <section className="searchBar w-4/5 mx-auto flex border-2">
+        <input className="searchInput flex-grow pl-4" type="text" placeholder="e.g. 'Newcastle'" spellCheck="false" />
+        <button className="searchButton p-4 bg-red-100" type="button">Search</button>
       </section>
-      <div className="error-message text-center"></div>
+      <div className="errorMessage text-center"></div>
     </section>
   )
 }
--- src/components/UserWeather.tsx
+++ src/components/UserWeather.tsx
@@ -1,6 +1,30 @@
+import { useEffect } from 'react';
+
 function UserWeather() {
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          //fetchWeatherData(latitude, longitude);
+        },
+        (error) => {
+          //setError('Error getting location');
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      //setError('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
   return (
-    <h1>UserWeather</h1>
+    <section className="w-11/12 h-72 mt-4 mx-auto justify-center border-2 border-red-200 flex items-center">
+      <section className="userLocation flex flex-col justify-center items-center">
+        <button>Allow Location Access</button>
+        <div>Allow location access to see your local weather, or search your city above manually.</div>
+      </section>
+    </section>
   )
 }
 
